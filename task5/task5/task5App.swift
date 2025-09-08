39c7b76793d203c15e919976e19ca1dcfefb79ee
İlknur Tulgar <ilknur001-@hotmail.com>
2025-09-08 11:43:24 +0300

    task5 started

---
+++ task5/task5/task5App.swift
@@ -0,0 +1,17 @@
+//
+//  task5App.swift
+//  task5
+//
+//  Created by İlknur Tulgar on 8.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct task5App: App {
+    var body: some Scene {
+        WindowGroup {
+            TaskManagementApp()
+        }
+    }
+}
