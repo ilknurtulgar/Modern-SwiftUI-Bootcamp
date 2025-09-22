b2abb9a1678f31d2d56057227e7e04c57c176679
İlknur Tulgar <ilknur001-@hotmail.com>
2025-09-23 00:16:47 +0300

    model has been established, other continue

--- task9/task9/Models/WeatherModel.swift
+++ task9/task9/Models/WeatherModel.swift
@@ -5,3 +5,23 @@
 //  Created by İlknur Tulgar on 22.09.2025.
 //
 
+import Foundation
+
+/*https://api.openweathermap.org/data/2.5/weather?q={CITY},{STATE_CODE},{COUNTRY_CODE}&appid={API_KEY}&units=metric&lang=tr
+ */
+
+struct WeatherResponse: Codable{
+    let cityName: String
+    let weather: [Weather]
+    let mainInfo: Main
+}
+
+struct Weather: Codable{
+    let description: String
+    let icon: String
+}
+
+struct Main: Codable {
+    let temp: Double
+    let humidity: Int
+}
--- task9/task9/ViewModels/WeatherViewModel.swift
+++ task9/task9/ViewModels/WeatherViewModel.swift
@@ -7,3 +7,11 @@
 
 import Foundation
 
+class WeatherViewModel: ObservableObject {
+    @Published var weather: WeatherResponse?
+    
+    func fetchWeather(for city: String) {
+        let apiKey = "f7091c7b4ebad85106a3d96dcd28ec8e"
+        let urlString = "https://api.openweathermap.org/data/2.5/weather?q=\(city)&appid=\(apiKey)&units=metric"
+    }
+}
